10f1ab101cbad15f11d2b2253f662bc2fb1d80c1
Elite Story <elitestoryproductions@gmail.com>
2026-04-30 16:49:17 +0530

    Auto-Sync from Mobile IDE

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/css/styles.css',
+  '/css/all.min.css',
   '/js/app.js',
   '/js/canvas-manager.js',
   '/js/image-editor.js',
@@ -11,13 +12,33 @@ const urlsToCache = [
   '/js/undo-redo.js',
   '/js/storage.js',
   '/js/file-upload.js',
-  '/lib/fabric.min.js'
+  '/lib/fabric.min.js',
+  '/lib/html2canvas.min.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
 });
